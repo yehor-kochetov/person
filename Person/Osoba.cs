4a18daf9c539c5d5c172bb9f3162e9342131b6db
Yehor Kochetov <3678031@teb-gdansk.ad>
2024-01-03 12:27:36 +0100

    first commit

---
+++ Person/Osoba.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Osoba
+{
+    public string Imie { get; set; }
+    public string Nazwisko { get; set; }
+    public char Plec { get; set; } // M dla mężczyzny, K dla kobiety
+    public DateTime DataUrodzenia { get; set; }
+
+    // Konstruktor
+    public Osoba(string imie, string nazwisko, char plec, DateTime dataUrodzenia)
+    {
+        Imie = imie;
+        Nazwisko = nazwisko;
+        Plec = plec;
+        DataUrodzenia = dataUrodzenia;
+    }
+}
---
+++ Person/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Przykład utworzenia obiektu klasy Osoba
+        Osoba osoba = new Osoba("Jan", "Kowalski", 'M', new DateTime(1990, 1, 1));
+        Console.WriteLine($"Imię: {osoba.Imie}, Nazwisko: {osoba.Nazwisko}, Płeć: {osoba.Plec}, Data Urodzenia: {osoba.DataUrodzenia.ToShortDateString()}");
+    }
+}
